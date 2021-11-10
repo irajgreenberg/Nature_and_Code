943ade098b100e20d800e1e1d52f2d68dd5fb198
Greenberg <irajgreenberg@gmail.com>
2021-11-10 11:50:18 -0500

    particle in a box

--- NatureandCode_Three/src/client/Particle.ts
+++ NatureandCode_Three/src/client/Particle.ts
@@ -1,8 +1,7 @@
 import { Color, Group, Mesh, MeshBasicMaterial, SphereBufferGeometry, Vector3 } from "three";
 
-const GRAVITY = .03;
+const GRAVITY = -.003;
 const DAMPING = .75;
-
 
 export class Particle extends Group {
     pos: Vector3;
@@ -22,7 +21,44 @@ export class Particle extends Group {
         const partGeom = new SphereBufferGeometry(this.radius);
         const partMat = new MeshBasicMaterial({ color: this.color });
         this.part = new Mesh(partGeom, partMat);
+        this.part.position.x = pos.x;
+        this.part.position.y = pos.y;
+        this.part.position.z = pos.z;
         this.add(this.part);
+    }
+
+    move(): void {
+        this.spd.y += GRAVITY;
+        this.part.position.x += this.spd.x;
+        this.part.position.y += this.spd.y;
+        this.part.position.z += this.spd.z;
+    }
+
+    collideBounds(bounds:Vector3){
+        if (this.part.position.x > bounds.x/2){
+            this.part.position.x = bounds.x/2
+            this.spd.x *=-1;
+        } else if (this.part.position.x < -bounds.x/2){
+            this.part.position.x = -bounds.x/2
+            this.spd.x *=-1;
+        }
+
+        if (this.part.position.y < -bounds.y/2){
+            this.part.position.y = -bounds.y/2
+            this.spd.y *=-1;
+        } else if (this.part.position.y > bounds.y/2){
+            this.part.position.y = bounds.y/2
+            this.spd.y *=-1;
+        }
+
+        if (this.part.position.z < -bounds.z/2){
+            this.part.position.z = -bounds.z/2
+            this.spd.z *=-1;
+        } else if (this.part.position.z > bounds.y/2){
+            this.part.position.z = bounds.y/2
+            this.spd.z *=-1;
+        }
+
     }
 
 }
--- NatureandCode_Three/src/client/client.ts
+++ NatureandCode_Three/src/client/client.ts
@@ -6,7 +6,7 @@ import { Particle } from './Particle'
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 2
+camera.position.z = 4
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -14,17 +14,18 @@ document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
-const geometry = new THREE.BoxGeometry()
+const geometry = new THREE.BoxGeometry(5, 3, 3)
 const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
     wireframe: true,
 })
-
 const cube = new THREE.Mesh(geometry, material)
+console.log(cube.geometry.parameters.width);
 scene.add(cube)
-
-const part = new Particle(new Vector3(0, 1.5, 0), .05, new Vector3(0, .02, 0), new Color(0x6622bb));
+let cubeBounds = new Vector3(cube.geometry.parameters.width, cube.geometry.parameters.height, cube.geometry.parameters.depth);
+const part = new Particle(new Vector3(0, 1.5, 0), .25, new Vector3(.2, .2, .2), new Color(0x6622bb));
 scene.add(part);
+
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -37,8 +38,10 @@ function onWindowResize() {
 function animate() {
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+    // cube.rotation.x += 0.01
+    // cube.rotation.y += 0.01
+    part.move();
+    part.collideBounds(cubeBounds);
 
     controls.update()
 
