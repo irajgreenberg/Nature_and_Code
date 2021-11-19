bdf93369c4581ac21bd6363cf3451dce6a6e857b
irajgreenberg <irajgreenberg@gmail.com>
2021-11-19 12:05:56 -0600

    resetting

--- NatureandCode_Three/src/client/NandCUtils.ts
+++
@@ -1,5 +0,0 @@
-//https://www.geeksforgeeks.org/how-to-generate-random-number-in-given-range-using-javascript/
-// returnts pseudo random float between min-max
-export function randRange(min:number, max:number):number { 
-    return Math.random() * (max - min) + min;
-} 
